108e9297679e2a86908fcba288c5d8900d6a6434
mamun8893 <0316361936@student.wub.edu.bd>
2022-04-06 20:48:15 +0600

    project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useEffect } from "react";
+
+const alanKey =
+  "f2c7f318124baaaae69a67b4743fdef82e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          console.log(articles);
+        }
+      },
+    });
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
